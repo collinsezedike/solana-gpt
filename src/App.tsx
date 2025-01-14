ef07e252ae9a15893a1fbcf0635d8ba581163a4b
ezedike evan <egwomevan323@gmail.com>
2025-01-14 11:49:41 +0100

    shorter hero message

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ const App = ()=>{
     <div>
       <Navbar />
       <h1>
-        Be empowered users with a seamless and secure authentication experience. 
-        Civical offers a modern, 
-        easy-to-integrate API that enhances your application's security while keeping things simple and efficient. 
+        Be empowered with a seamless and secure authentication experience. 
+        Civical enhances security while keeping things simple and efficient. 
         Say goodbye to clunky login flows and hello to smooth, frictionless authentication — 
         all powered by industry-leading technology.
-        Start building smarter. Start with Civical.
       </h1>
     </div>
   )
