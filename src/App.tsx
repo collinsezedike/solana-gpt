9995d4256204110acf14089f06b0326d88301551
ezedike evan <egwomevan323@gmail.com>
2025-01-14 11:40:23 +0100

    'responsiveness'

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import Navbar from './components/navBar'
 const App = ()=>{
 
   return (
+    <div>
       <Navbar />
+      <h1>
+        Be empowered users with a seamless and secure authentication experience. 
+        Civical offers a modern, 
+        easy-to-integrate API that enhances your application's security while keeping things simple and efficient. 
+        Say goodbye to clunky login flows and hello to smooth, frictionless authentication — 
+        all powered by industry-leading technology.
+        Start building smarter. Start with Civical.
+      </h1>
+    </div>
   )
 }
 
